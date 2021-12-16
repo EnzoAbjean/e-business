1ee57940002a8c802ff5e1719fc98798ff06e3cc
Abjean Enzo <enzo.abjean@etud.fr>
2021-12-16 11:14:11 +0100

    Test Bd

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+//const axios = require('axios')
 const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
 
@@ -6,6 +7,17 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
+app.get('/kill',function(req,res){
+    res.send("BBOUUUUMMMMM");
+
+})
+app.get('/MesRecettes', async function(req, res) {
+    res.send("gola");
+})
+
 app.listen(PORT, function () {
     console.log('Example app listening on port ' + PORT)
 })
+
+
+
